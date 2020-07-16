62da8440a89f7d1f26f823cfde109856b91e0102
Tan <nttan.env@gmail.com>
2020-07-16 16:06:35 +0700

    update route

--- Route/users.route.js
+++ Route/users.route.js
@@ -3,10 +3,11 @@ var router = express.Router();
 var controller = require("../controllers/users.controller");
 var validation = require("../validation/users.validation");
 
+
 router.get("/", controller.index);
-router.get("/update/:name", controller.update);
-router.post("/update/:name", controller.postUpdate);
-router.post("/delete/:name", controller.postDelete);
+router.get("/update/:id", controller.update);
+router.post("/update/:id", controller.postUpdate);
+router.post("/delete/:id", controller.postDelete);
 router.get("/create", controller.create);
 router.post("/create", validation.postCreate, controller.postCreate); //middle ware
 
