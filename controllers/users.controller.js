c71f3d45a4af4dbcc8f44e10099b8d61d768965d
Tan <nttan.env@gmail.com>
2020-07-16 16:07:21 +0700

    update user controller

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -7,27 +7,36 @@ module.exports.index = function(req, res) {
 } 
 
 module.exports.update = function(req, res) {
-  res.render("./users/update", { name: req.params.name });
+  var user = db.get("users")
+              .find({ id: req.params.id })
+              .value();
+  res.render("./users/update", { oldname: user.name,
+                                  id: user.id
+                                  });
 }
+
 module.exports.postUpdate = function(req, res) {
-  req.body.id = shortid.generate();
   db.get("users")
-    .find({ name: req.params.name })
+    .find({ id: req.params.id })
     .assign(req.body)
     .write();
   res.redirect("/users");
 }
+
 module.exports.postDelete = function(req, res) {
   db.get("users")
-    .remove({ name: req.params.name })
+    .remove({ id: req.params.id })
     .write();
   res.redirect("/users");
 }
+
 module.exports.create = function(req, res) {
   res.render("./users/create");
 }
+
 module.exports.postCreate = function(req, res) {
   req.body.id = shortid.generate();
+  req.body.password = "123123";
   db.get("users")
     .push(req.body)
     .write();
