96e9b06acb4f41bf706dd198b70b8fa53d935d05
Tan <nttan.env@gmail.com>
2020-07-22 16:44:20 +0700

    add new feature: Pagination

---
+++ controllers/products.controller.js
@@ -0,0 +1,15 @@
+var db = require("../db");
+
+
+
+module.exports.index = function(req, res) {
+	var page = parseInt(req.query.page) || 1;// n 
+	var perPage = 8;
+	
+	var start = (page - 1)*perPage;
+	var end = page* perPage;
+
+  res.render("products/products", {
+  	products: db.get('products').value().slice(start, end)
+  });
+}
