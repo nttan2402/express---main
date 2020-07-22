41d5515bea7921f2af9abfb8c343b89a72741d5b
nttan2402 <nttan.env@gmail.com>
2020-07-22 22:02:07 +0700

    finish pagination

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -3,13 +3,26 @@ var db = require("../db");
 
 
 module.exports.index = function(req, res) {
-	var page = parseInt(req.query.page) || 1;// n 
+	var page = parseInt(req.query.page) || 1;// n(2)
 	var perPage = 8;
 	
-	var start = (page - 1)*perPage;
-	var end = page* perPage;
+	var start = (page - 1)*perPage;//0-8
+	var end = page* perPage;//8-16
 
-  res.render("products/products", {
-  	products: db.get('products').value().slice(start, end)
-  });
+	if(page === 1) {
+		 res.render("products/products", {
+		  	products: db.get('products').value().slice(start, end),
+		  	pagePrevious: page,
+		  	page: page + 1,
+		  	pageNext: page + 2
+		  });
+		}else {
+		  res.render("products/products", {
+		  	products: db.get('products').value().slice(start, end),
+		  	pagePrevious: page -1,
+		  	page: page,
+		  	pageNext: page + 1
+		  });
+		}
+
 }
